4d004b6b52eec0a6be9b3bfaab23fe4f811f827e
HarryPerez <c-feperez@globalization-partners.com>
2021-11-11 18:38:47 -0300

    test

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://go-surfing-ui.herokuapp.com:3000', credentials: true }));
 app.use(express.json());
 
 app.use(
