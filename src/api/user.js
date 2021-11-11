768262400fa690d96dbe5bd2d9673d2c3795da3c
HarryPerez <c-feperez@globalization-partners.com>
2021-11-11 19:11:42 -0300

    some fixes

--- src/api/user.js
+++ src/api/user.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const User = require('../models/user');
-const { isUserAuthenticated } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.get('/auth/user', isUserAuthenticated, (req, res) => {
-  console.log('USER IN REQUEST', req);
+router.get('/auth/user', (req, res) => {
   res.json(req.user);
 });
 
--- src/app.js
+++ src/app.js
@@ -29,7 +29,9 @@ app.use(
   cookieSession({
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_KEY],
-    sameSite: 'none'
+    sameSite: 'none',
+    secure: false,
+    httpOnly: false
   })
 );
 
