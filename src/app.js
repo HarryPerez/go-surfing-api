07ee0bfbe4c320ef6340817b5566b6a6029fb301
HarryPerez <c-feperez@globalization-partners.com>
2021-11-11 17:24:08 -0300

    another fix

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(express.json());
 
 app.use(
