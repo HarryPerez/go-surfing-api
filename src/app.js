a5808678cebdfb9efcc9b5dfa57bc8b63f5405e4
HarryPerez <c-feperez@globalization-partners.com>
2021-11-11 18:10:10 -0300

    test

--- src/app.js
+++ src/app.js
@@ -30,7 +30,11 @@ app.use(
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_KEY],
     httpOnly: false,
-    sameSite: 'none'
+    sameSite: 'none',
+    cookie: {
+      secure: true,
+      sameSite: 'None'
+    }
   })
 );
 
